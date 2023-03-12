987068a2b03c9b137fe0773c3746b3f70a6f02b2
Stephen <stevenmolvin@gmail.com>
2023-03-13 00:17:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const element = document.getElementById("main");
 element.remove("main");
 const newHeader = document.createElement("h1");
-//creating h1 id
+
 newHeader.id= "victory"
-//creating innerHtml inside h1
+
 newHeader.innerHTML = "Molvin is the champion"
